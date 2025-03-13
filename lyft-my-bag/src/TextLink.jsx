6e13673adb8123be1a93db01add526c3bbcb51c1
Savannah Ogletree <bff8078@gmail.com>
2025-03-13 19:21:18 -0400

    imported Figma to HTML for Home page semi

---
+++ lyft-my-bag/src/TextLink.jsx
@@ -0,0 +1,16 @@
+﻿import PropTypes from "prop-types";
+import React from "react";
+import "./style.css";
+
+export const TextLink = ({ text = "Text Link", className }) => {
+    return (
+        <div className={`text-link ${className}`}>
+            <div className="text-wrapper">{text}</div>
+        </div>
+    );
+};
+
+TextLink.propTypes = {
+    text: PropTypes.string,
+};
+
