cd83e4449ad894bb00db833547a7303aa082709e
Bhavana <148469250+BhavanaK223@users.noreply.github.com>
2025-03-27 16:54:37 -0400

    Setting up server

--- lyft-my-bag/src/Login.jsx
+++ lyft-my-bag/src/Login.jsx
@@ -1,10 +1,11 @@
-﻿import React from "react";
+﻿import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { TextLink } from "./TextLink";
 import gatorBlur2 from "./public/gator-blur-2.png";
 import "./style.css";
 
 export const LoginPage = () => {
+
     return (
         <div className="login-page">
             <div className="form-log-in">
@@ -13,11 +14,32 @@ export const LoginPage = () => {
                 <div className="div">Welcome Back!</div>
                 <Link to="/">Go to Home Page</Link>
 
-                <p className="p">Please enter your information below:</p>
+                <p>Please enter your information below:</p>
+                
 
-                <p className="p">Please enter your information below:</p>
+                <form action="" method="post">
+                <div class="form-group">
+                        <label for="Fullname">Full name</label>
+                        <input name="fullname" class="form-control" id="inputFullName" aria-describedby="emailHelp" placeholder="Enter full name"/>
+                </div>
+                <div class="form-group">
+                        <label for="InputEmail">Email address</label>
+                        <input name="email" class="form-control" id="InputEmail"  placeholder="Enter email"/>
+                </div>
+                <div class="form-group">
+                    <label for="InputPassword">Password</label>
+                    <input type="password" name="password1" class="form-control" id="InputPassword" placeholder="Password"/>
+                </div>
 
-                {/* <div className="input-field">
+                    <div class="form-group">
+                    <label for="InputPassword2">Repeat Password</label>
+                    <input type="password" name="password2" class="form-control" id="InputPassword2" placeholder="Repeat Password"/>
+
+                    </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+{/* 
+                <div className="input-field">
                     <label for="username">Username</label>
                     <input type="text" id="username" placeholder="example@gmail.com"></input>
                     <label for="password">Password</label>
@@ -27,28 +49,6 @@ export const LoginPage = () => {
 
                     </div>
                 </div> */}
-
-                <div className="input-field">
-                    <div className="label">Email</div>
-
-                    <div className="input">
-                        <div className="text-wrapper-2">Value</div>
-                    </div>
-                </div>
-
-                <div className="input-field">
-                    <div className="label">Password</div>
-
-                    <div className="input">
-                        <div className="text-wrapper-2">Value</div>
-                    </div>
-                </div>
-
-                <div className="button-group">
-                    <button className="button">
-                        <button className="button-2">Sign In</button>
-                    </button>
-                </div>
 
                 <TextLink className="text-link-instance" text="Forgot password?" />
             </div>
