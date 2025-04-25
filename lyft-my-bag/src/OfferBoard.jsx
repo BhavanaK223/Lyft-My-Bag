f54bbab7989f5714dbd5b752cc76b8a8b0cf97bf
jiangcctv <138551486+jiangcctv@users.noreply.github.com>
2025-04-25 02:00:31 -0400

    offer board style update

--- lyft-my-bag/src/OfferBoard.jsx
+++ lyft-my-bag/src/OfferBoard.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Navbar } from "./Navbar";
-import "./style.css";
+import "./OfferBoard.css";
 import { TextLink } from "./TextLink";
+import { TripDisplay } from "./TripDisplay";
+import figure6 from "./figure-6.png"
 
 export const OfferBoard = () => {
     const [trips, setTrips] = useState([]);
@@ -91,11 +93,109 @@ export const OfferBoard = () => {
     };
     
     return(
-        <div className="page">
-            {/*<div><Navbar /></div>*/}
+        <div className="available-rides">
+            <div className="overlap-wrapper">
+                <div className="overlap">
+                    <header className="header">
+                        <div className="overlap-group-3">
+                            <img className="header-img" alt="green wavy header" src={figure6} />
+
+                            <div className="text-wrapper-3">LMB</div>
+
+                            <div className="header-buttons">
+                                <button className="dashboard-button">
+                                    <a href="/profile" className="text-wrapper-4">Dashboard</a>
+                                </button>
+
+                                <button className="create-button">
+                                    <a href="/request" className="text-wrapper-5">Create New Trip</a>
+                                </button>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div className="sidebar">
+                        <div className="search-bar">
+                            <div className="state-layer-2">
+                                <div className="content">
+                                    <div className="supporting-text">Search available rides</div>
+                                </div>
+
+                                {/* search icon here */}
+                            </div>
+                        </div>
+
+                        <div className="filters-destination">
+                            Filters <br />
+                            Destination Type <br />
+                            Date <br />
+                            Time <br />
+                            Duration <br />
+                            Gas Money
+                        </div>
+                    </div>
+
+                    <div className="results-section">
+                        <div className="text-wrapper-2">Available Rides</div>
+
+                        {trips.length === 0 ? (
+                            <div className="button-group">
+                                <p className="p">No trips yet</p>
+                                        <Link to="/login" className="login-button">
+                                            <div className="text-wrapper-4">Create New Trip</div>
+                                        </Link>
+                                                                        
+                                    </div>
+                        ) : (
+                            <div className="grid">
+                            {trips.map((trip) => (
+                                <div key={trip._id} className="card profile-card">
+                                    <h2 className="text-lg font-semibold mb-1">{trip.destinationName}</h2>
+                                    <p>{trip.address}</p>
+                                    <p><strong>{trip.firstName} {trip.lastName?.charAt(0)}</strong></p>
+                                    {/* <p><strong>Type:</strong> {trip.destinationType}</p> */}
+                                    <p><strong>Date:</strong> {trip.date} @ {trip.time}</p>
+                                    <p><strong>Duration:</strong> {trip.duration} Hours</p>
+                                    
+                                    <p><strong>Seats:</strong> {trip.seatsAvailable}</p>
+                                    <p><strong>Compensation:</strong> ${trip.compensation}</p>
+                                    
+                                    {/* <Link to="/profile" className="login-button">
+                                        <div className="text-wrapper-4">Request</div>
+                                    </Link> */}
+                                    {trip.email !== userEmail && // not the trip owner
+                                    !trip.riders?.includes(userEmail) && // not already joined
+                                    trip.seatsAvailable > 0 && ( // trip not full
+                                        <button onClick={() => handleJoinTrip(trip._id)} className="login-button"> 
+                                            Join Trip
+                                        </button>
+                                    )}
+                                    {trip.riders?.includes(userEmail) && (
+                                        <div>
+                                            <p>You have joined this trip!</p>
+                                        <button onClick={() => handleLeaveTrip(trip._id)} className="login-button">
+                                            Leave Trip
+                                        </button>
+                                        </div>
+                                        
+                                    )}
+                                    {trip.email === userEmail && (
+                                        <p>You created this trip!</p>
+                                    )}
+
+                                    {trip.additionalNotes && (
+                                        <p className="italic mt-2 text-gray-600">Note: {trip.additionalNotes}</p>
+                                    )}
+                                </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
             <div className="p-4">
                 <h1 className="text-2xl font-bold mb-4">Offer Board</h1>
-                {trips.length === 0 ? (
+                {/* {trips.length === 0 ? (
                     <div className="button-group">
                         <p className="p">No trips yet</p>
                                 <Link to="/login" className="login-button">
@@ -107,46 +207,46 @@ export const OfferBoard = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     {trips.map((trip) => (
                         <div key={trip._id} className="card profile-card">
-                        <h2 className="text-lg font-semibold mb-1">{trip.destinationName}</h2>
-                        <p>{trip.address}</p>
-                        <p><strong>{trip.firstName} {trip.lastName?.charAt(0)}</strong></p>
-                        {/* <p><strong>Type:</strong> {trip.destinationType}</p> */}
-                        <p><strong>Date:</strong> {trip.date} @ {trip.time}</p>
-                        <p><strong>Duration:</strong> {trip.duration} Hours</p>
-                        
-                        <p><strong>Seats:</strong> {trip.seatsAvailable}</p>
-                        <p><strong>Compensation:</strong> ${trip.compensation}</p>
-                        
-                        {/* <Link to="/profile" className="login-button">
-                            <div className="text-wrapper-4">Request</div>
-                        </Link> */}
-                        {trip.email !== userEmail && // not the trip owner
-                        !trip.riders?.includes(userEmail) && // not already joined
-                        trip.seatsAvailable > 0 && ( // trip not full
-                            <button onClick={() => handleJoinTrip(trip._id)} className="login-button"> 
-                                Join Trip
-                            </button>
-                        )}
-                        {trip.riders?.includes(userEmail) && (
-                            <div>
-                                <p>You have joined this trip!</p>
-                            <button onClick={() => handleLeaveTrip(trip._id)} className="login-button">
-                                Leave Trip
-                            </button>
-                            </div>
+                            <h2 className="text-lg font-semibold mb-1">{trip.destinationName}</h2>
+                            <p>{trip.address}</p>
+                            <p><strong>{trip.firstName} {trip.lastName?.charAt(0)}</strong></p>
+                            {/* <p><strong>Type:</strong> {trip.destinationType}</p> 
+                            <p><strong>Date:</strong> {trip.date} @ {trip.time}</p>
+                            <p><strong>Duration:</strong> {trip.duration} Hours</p>
                             
-                        )}
-                        {trip.email === userEmail && (
-                            <p>You created this trip!</p>
-                        )}
+                            <p><strong>Seats:</strong> {trip.seatsAvailable}</p>
+                            <p><strong>Compensation:</strong> ${trip.compensation}</p>
+                            
+                            {/* <Link to="/profile" className="login-button">
+                                <div className="text-wrapper-4">Request</div>
+                            </Link> 
+                            {trip.email !== userEmail && // not the trip owner
+                            !trip.riders?.includes(userEmail) && // not already joined
+                            trip.seatsAvailable > 0 && ( // trip not full
+                                <button onClick={() => handleJoinTrip(trip._id)} className="login-button"> 
+                                    Join Trip
+                                </button>
+                            )}
+                            {trip.riders?.includes(userEmail) && (
+                                <div>
+                                    <p>You have joined this trip!</p>
+                                <button onClick={() => handleLeaveTrip(trip._id)} className="login-button">
+                                    Leave Trip
+                                </button>
+                                </div>
+                                
+                            )}
+                            {trip.email === userEmail && (
+                                <p>You created this trip!</p>
+                            )}
 
-                        {trip.additionalNotes && (
-                            <p className="italic mt-2 text-gray-600">Note: {trip.additionalNotes}</p>
-                        )}
+                            {trip.additionalNotes && (
+                                <p className="italic mt-2 text-gray-600">Note: {trip.additionalNotes}</p>
+                            )}
                         </div>
                         ))}
                     </div>
-                )}
+                )} */}
             </div>
         </div>
         
